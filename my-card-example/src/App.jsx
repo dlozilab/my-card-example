1fb8077eb8b2ca0c5ae3af1bdb948913e5b1da0f
dlozilab <dlozi@mlab.co.za>
2024-07-31 09:24:00 +0200

    feat: create vite app

---
+++ my-card-example/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import PropertyCard from './components/propertyCard'
+
+function App() {
+
+
+  return (
+    <>
+      <PropertyCard/>
+    </>
+  )
+}
+
+export default App
