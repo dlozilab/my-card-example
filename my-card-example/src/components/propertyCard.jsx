d44bff88a162b096702abac6294f561a0ba6ee1b
dlozilab <dlozi@mlab.co.za>
2024-07-31 09:54:49 +0200

    feat: comp propertyCard

--- my-card-example/src/components/propertyCard.jsx
+++ my-card-example/src/components/propertyCard.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function PropertyCard() {
   return (
-    <div style={{backgroundColor:"red"}}>P</div>
+    <div style={{backgroundColor:"red", width:"40%",height:"70%",borderRadius:"15px",boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
+        <section style={{}}></section>
+        <section></section>
+        <section></section>
+        <section></section>
+    </div>
   )
 }
